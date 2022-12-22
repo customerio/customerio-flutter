4345a11734409b718d7533e8733c001a243a33f4
Shahroz Khan <shrozali@gmail.com>
2022-12-22 12:43:48 -0500

    added code samples (#8)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:customer_io/customer_io.dart';
 import 'package:customer_io/customer_io_config.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +5,7 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await CustomerIo.initialize(
+  await CustomerIO.initialize(
     config: CustomerIOConfig(
       siteId: "YOUR_SITE_ID",
       apiKey: "YOUR_API_KEY",
@@ -25,40 +23,78 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-    CustomerIo.identify(
-        identifier: "and-flut-wow",
-        attributes: {"name": "yolo", "age": 10, "weight": 33.3});
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = "Platform";
-    });
+    CustomerIO.identify(
+        identifier: "flutter-example",
+        attributes: {"name": "Flutter CIO", "email": "example@flutter.io"});
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Plugin example app'),
+          ),
+          body: Column(
+            children: <Widget>[
+              const Spacer(),
+              Center(
+                child: ElevatedButton(
+                  child: const Text('TRACK EVENT'),
+                  onPressed: () {
+                    CustomerIO.track(name: "ButtonClick", attributes: {
+                      'stringType': 'message',
+                      'numberType': 123,
+                      'booleanType': true,
+                    });
+                  },
+                ),
+              ),
+              const Spacer(),
+              Center(
+                child: ElevatedButton(
+                  child: const Text('TRACK SCREEN'),
+                  onPressed: () {
+                    CustomerIO.screen(name: "LoginScreen", attributes: {
+                      "loggedIn": false,
+                    });
+                  },
+                ),
+              ),
+              const Spacer(),
+              Center(
+                child: ElevatedButton(
+                  child: const Text('SET DEVICE ATTRIBUTES'),
+                  onPressed: () {
+                    CustomerIO.setDeviceAttributes(
+                        attributes: {"wifi_connected": true, "region": "us"});
+                  },
+                ),
+              ),
+              const Spacer(),
+              Center(
+                child: ElevatedButton(
+                  child: const Text('SET PROFILE ATTRIBUTES'),
+                  onPressed: () {
+                    CustomerIO.setProfileAttributes(
+                        attributes: {"age": 31, "height": 5.9, "gender": "M"});
+                  },
+                ),
+              ),
+              const Spacer(),
+              Center(
+                child: ElevatedButton(
+                    child: const Text('CLEAR IDENTITY'),
+                    onPressed: () {
+                      CustomerIO.clearIdentify();
+                    }),
+              ),
+              const Spacer(),
+            ],
+          )),
     );
   }
 }
