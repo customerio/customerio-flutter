cb873a57af4fd91495287453c9dac936658a89aa
Shahroz Khan <shrozali@gmail.com>
2023-03-06 15:11:46 +0500

    added mock attempt for plugin and methodchannel

--- test/customer_io_method_channel_test.dart
+++ test/customer_io_method_channel_test.dart
@@ -1,3 +1,5 @@
+import 'package:customer_io/customer_io_config.dart';
+import 'package:customer_io/customer_io_method_channel.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -8,11 +10,26 @@ void main() {
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+      switch (methodCall.method) {
+        case 'initialize':
+          return null; // Simulate a successful response from the platform.
+        default:
+          throw MissingPluginException();
+      }
     });
   });
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
+  });
+
+  test('initialize should call platform method', () async {
+    final customerIO = CustomerIOMethodChannel();
+    final config = CustomerIOConfig(
+      siteId: 'site_id',
+      apiKey: 'api_key'
+    );
+    await customerIO.initialize(config: config);
+    expect(channel, isMethodCall('initialize', arguments: config.toMap()));
   });
 }
--- test/customer_io_test.dart
+++ test/customer_io_test.dart
@@ -1,87 +1,35 @@
-import 'dart:async';
-
+import 'package:customer_io/customer_io.dart';
 import 'package:customer_io/customer_io_config.dart';
-import 'package:customer_io/customer_io_enums.dart';
-import 'package:customer_io/customer_io_inapp.dart';
 import 'package:customer_io/customer_io_method_channel.dart';
 import 'package:customer_io/customer_io_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 ///The MockCustomerIoPlatform class is a mock implementation of the CustomerIOPlatform interface.
 /// It provides stubbed implementations of all the methods defined in the CustomerIOPlatform interface,
 /// which are intended to be overridden in tests. The purpose of this class is to simulate the behavior
 /// of the actual platform implementation, so that tests can run without making actual calls to the platform,
-class MockCustomerIoPlatform
-    with MockPlatformInterfaceMixin
-    implements CustomerIOPlatform {
-  @override
-  Future<void> initialize({required CustomerIOConfig config}) {
-    // TODO: implement config
-    throw UnimplementedError();
-  }
-
-  @override
-  void clearIdentify() {
-    // TODO: implement clearIdentify
-  }
-
-  @override
-  void identify(
-      {required String identifier,
-      Map<String, dynamic> attributes = const {}}) {
-    // TODO: implement identify
-  }
-
-  @override
-  void screen(
-      {required String name, Map<String, dynamic> attributes = const {}}) {
-    // TODO: implement screen
-  }
-
-  @override
-  void setDeviceAttributes({required Map<String, dynamic> attributes}) {
-    // TODO: implement setDeviceAttributes
-  }
-
-  @override
-  void setProfileAttributes({required Map<String, dynamic> attributes}) {
-    // TODO: implement setProfileAttributes
-  }
-
-  @override
-  void track(
-      {required String name, Map<String, dynamic> attributes = const {}}) {
-    // TODO: implement track
-  }
-
-  @override
-  StreamSubscription subscribeToInAppEventListener(
-      void Function(InAppEvent p1) onEvent) {
-    // TODO: implement subscribeToInAppEventListener
-    throw UnimplementedError();
-  }
-
-  @override
-  void registerDeviceToken({required String deviceToken}) {
-    // TODO: implement registerDeviceToken
-  }
-
-  @override
-  void trackMetric(
-      {required String deliveryID,
-      required String deviceToken,
-      required MetricEvent event}) {
-    // TODO: implement trackMetric
-  }
-}
+class MockCustomerIoPlatform extends Mock
+    with MockPlatformInterfaceMixin  implements CustomerIOPlatform{ }
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  final CustomerIOPlatform initialPlatform = CustomerIOPlatform.instance;
 
-  test('$CustomerIOMethodChannel is the default instance', () {
-    expect(initialPlatform, isInstanceOf<CustomerIOMethodChannel>());
+  group('CustomerIO', () {
+    late MockCustomerIoPlatform mockPlatform;
+
+    setUp(() {
+      mockPlatform = MockCustomerIoPlatform();
+      CustomerIOPlatform.instance = mockPlatform;
+    });
+
+    test('initialize() calls platform', () async {
+      final config = CustomerIOConfig(siteId: '123', apiKey: '456');
+      when(mockPlatform.initialize(config: config)).thenAnswer((_) => Future.value());
+      await CustomerIO.initialize(config: config);
+      verify(mockPlatform.initialize(config: config)).called(1);
+    });
   });
 }
