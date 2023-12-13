0fba64c99984ac0bbdf284dbfa13e260510839d3
Levi Bostian <levi@customer.io>
2023-12-13 14:08:21 -0600

    install firebase sdks

--- apps/amiapp_flutter/lib/main.dart
+++ apps/amiapp_flutter/lib/main.dart
@@ -4,8 +4,17 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'src/app.dart';
 import 'src/auth.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize FCM (flutter-fire)
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   // Load SDK configurations
   await dotenv.load(fileName: ".env");
   // Wait for user state to be updated
