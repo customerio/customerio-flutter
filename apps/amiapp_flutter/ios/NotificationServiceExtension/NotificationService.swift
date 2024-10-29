23b5b689b1b01e7648075d8575b257c20199d13f
Rehan <mrehan.official@gmail.com>
2024-10-29 11:44:44 +0500

    chore: update iOS native dependencies to Data Pipelines

--- apps/amiapp_flutter/ios/NotificationServiceExtension/NotificationService.swift
+++ apps/amiapp_flutter/ios/NotificationServiceExtension/NotificationService.swift
@@ -4,7 +4,6 @@
 //
 
 import CioMessagingPushFCM
-import CioTracking
 
 class NotificationService: UNNotificationServiceExtension {
     
@@ -13,11 +12,14 @@ class NotificationService: UNNotificationServiceExtension {
     
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         print("NotificationService didReceive called")
-        
+
+        // TODO: Fix SDK initialization
+        /*
         CustomerIO.initialize(siteId: Env.siteId, apiKey: Env.apiKey, region: .US) { config in
             config.autoTrackDeviceAttributes = true
             config.logLevel = .debug
         }
+         */
         
         MessagingPush.shared.didReceive(request, withContentHandler: contentHandler)
     }
