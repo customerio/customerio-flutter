7dbc20425bcece87b181e4cafbd30614c618c65d
Rehan <mrehan.official@gmail.com>
2026-04-08 01:12:15 +0500

    update ios spm setup and ignore spm artifacts

--- ios/customer_io/Package.swift
+++ ios/customer_io/Package.swift
@@ -5,20 +5,24 @@ import PackageDescription
 let package = Package(
     name: "customer_io",
     platforms: [
-        .iOS("13.0")
+        .iOS("15.0")
     ],
     products: [
         .library(name: "customer-io", targets: ["customer_io"])
     ],
     dependencies: [
-        .package(url: "https://github.com/customerio/customerio-ios.git", exact: "4.4.0")
+        .package(url: "https://github.com/customerio/customerio-ios.git", exact: "4.4.0"),
+        .package(url: "https://github.com/customerio/customerio-ios-fcm.git", from: "1.0.0")
     ],
     targets: [
         .target(
             name: "customer_io",
             dependencies: [
                 .product(name: "DataPipelines", package: "customerio-ios"),
-                .product(name: "MessagingInApp", package: "customerio-ios")
+                .product(name: "MessagingInApp", package: "customerio-ios"),
+                .product(name: "Location", package: "customerio-ios"),
+                .product(name: "MessagingPushFCM", package: "customerio-ios"),
+                .product(name: "CioFirebaseWrapper", package: "customerio-ios-fcm")
             ]
         )
     ]
