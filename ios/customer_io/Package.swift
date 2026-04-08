fbd51d31b5526a1bf3f691cf994eda05a8d4bf21
Shahroz Khan <shrozali@gmail.com>
2026-04-08 11:47:05 -0400

    modules optional with flags

--- ios/customer_io/Package.swift
+++ ios/customer_io/Package.swift
@@ -1,6 +1,24 @@
 // swift-tools-version: 5.9
 
 import PackageDescription
+import Foundation
+
+// Check if the Location module should be included.
+// Customers can opt in by setting the environment variable:
+//   CIO_LOCATION=true
+// e.g., in Xcode: CIO_LOCATION=true flutter build ios
+let useLocation = ProcessInfo.processInfo.environment["CIO_LOCATION"]?.lowercased() == "true"
+
+var targetDependencies: [Target.Dependency] = [
+    .product(name: "DataPipelines", package: "customerio-ios"),
+    .product(name: "MessagingInApp", package: "customerio-ios"),
+    .product(name: "MessagingPushFCM", package: "customerio-ios"),
+    .product(name: "CioFirebaseWrapper", package: "customerio-ios-fcm")
+]
+
+if useLocation {
+    targetDependencies.append(.product(name: "Location", package: "customerio-ios"))
+}
 
 let package = Package(
     name: "customer_io",
@@ -17,13 +35,7 @@ let package = Package(
     targets: [
         .target(
             name: "customer_io",
-            dependencies: [
-                .product(name: "DataPipelines", package: "customerio-ios"),
-                .product(name: "MessagingInApp", package: "customerio-ios"),
-                .product(name: "Location", package: "customerio-ios"),
-                .product(name: "MessagingPushFCM", package: "customerio-ios"),
-                .product(name: "CioFirebaseWrapper", package: "customerio-ios-fcm")
-            ]
+            dependencies: targetDependencies
         )
     ]
 )
