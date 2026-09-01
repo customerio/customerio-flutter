536eb394b3f3a9570adda0eebc825d4f068fb6b8
Shahroz Khan <shrozali@gmail.com>
2026-09-01 17:36:51 -0400

    ci: clarify analyzer coverage

--- apps/flutter_sample_spm/lib/src/app.dart
+++ apps/flutter_sample_spm/lib/src/app.dart
@@ -1,5 +1,4 @@
 import 'package:customer_io/customer_io.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
